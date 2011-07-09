58cd327a7feba942cf38ae5ff3f6fe2aeb10fad7
Timmy Wilson <timmyt@smarttypes.org>
2011-07-09 15:04:00 -0400

    changed run_pmf.sh to work w/ new ec2 instance

--- smarttypes/graphlab/run_pmf.sh
+++ smarttypes/graphlab/run_pmf.sh
@@ -1,4 +1,4 @@
-cd /home/timmyt/graphlabapi/release/demoapps/pmf/
+cd /home/ubuntu/graphlabapi/release/demoapps/pmf/
 
 ./pmf smarttypes_pmf 1 --float=true --scheduler="round_robin(max_iterations=30,block_size=1)" \
 --burn_in=10 --ncpus=4 --binaryoutput=true --zero=true --D=2 --minval=0 --maxval=1
