d8a70f0e512930adcbb5cdcd4c57d852c857c9f4
Timmy Wilson <timmyt@smarttypes.org>
2011-07-13 20:37:57 -0400

    Getting ready to run first large graphlab job on ec2

--- smarttypes/graphlab/run_pmf.sh
+++ smarttypes/graphlab/run_pmf.sh
@@ -1,6 +1,6 @@
 cd /home/ubuntu/graphlabapi/release/demoapps/pmf/
 
 ./pmf smarttypes_pmf 1 --float=true --scheduler="round_robin(max_iterations=30,block_size=1)" \
---burn_in=10 --ncpus=4 --binaryoutput=true --zero=true --D=2 --minval=0 --maxval=1
+--burn_in=10 --ncpus=4 --binaryoutput=true --zero=true --D=66 --minval=0 --maxval=1
 
-mv smarttypes_pmf2.out /home/timmyt/projects/smarttypes/smarttypes/graphlab/smarttypes_pmf.out
+mv smarttypes_pmf66.out /home/timmyt/projects/smarttypes/smarttypes/graphlab/smarttypes_pmf.out
