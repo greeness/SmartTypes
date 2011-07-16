605281d2e2fd8e41b705e1f6778ef5c34bf4adb0
Timmy Wilson <timmyt@smarttypes.org>
2011-07-16 10:01:02 -0400

    tweaking run_pmf.sh

--- smarttypes/graphlab/run_pmf.sh
+++ smarttypes/graphlab/run_pmf.sh
@@ -6,7 +6,7 @@ cd /home/ubuntu/graphlabapi/release/demoapps/pmf/
 
 ./pmf smarttypes_pmf $METHOD --float=true --scheduler="round_robin(max_iterations=30,block_size=1)" \
 --burn_in=10 --ncpus=7 --binaryoutput=true --zero=true --D=$GROUPS \
-> pmf_method$METHOD_job.txt &
+> pmf_job.`/bin/date +"%Y_%m_%d_%H"`.txt &
 
 #--minval=0 --maxval=1
 #mv smarttypes_pmf$GROUPS.out /home/timmyt/projects/smarttypes/smarttypes/graphlab/smarttypes_pmf.out
