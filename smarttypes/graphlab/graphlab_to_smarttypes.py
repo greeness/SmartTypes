319e5348a908e4508cf933cca38b2ff89a17d7ac
Timmy Wilson <timmyt@smarttypes.org>
2011-07-16 21:01:13 -0400

    fixin

--- smarttypes/graphlab/graphlab_to_smarttypes.py
+++ smarttypes/graphlab/graphlab_to_smarttypes.py
@@ -4,6 +4,7 @@ We're running this on a remote ec2 instance
 """
 
 #tunnel mongo traffic through ssh
+#problem -- http://www.learnosity.com/techblog/index.cfm/2008/2/18/SSH-Portforward--Address-already-in-use--Solved
 import subprocess
 subprocess.call('ssh timmyt@66.228.60.238 -N -f -L 27017:localhost:27017', shell=True)
 
@@ -55,25 +56,25 @@ index_to_twitter_id_dict = pickle.load(open('index_to_twitter_id.pickle', 'r'))
 user_group_map = {} #{'id':([following], [followedby])}
 
 #TwitterGroup.bulk_delete('all')
-#for i in range(num_features):
-    #group_followers = []
-    #group_following = []
-    #for j in range(num_items):
-        #user_id = index_to_twitter_id_dict[j]
-        #follower_score = users_data[i][j]
-        #following_score = items_data[i][j]
+for i in range(num_features):
+    group_followers = []
+    group_following = []
+    for j in range(num_items):
+        user_id = index_to_twitter_id_dict[j]
+        follower_score = users_data[i][j]
+        following_score = items_data[i][j]
         
-        #group_followers.append((follower_score, user_id))
-        #group_following.append((following_score, user_id))
+        group_followers.append((follower_score, user_id))
+        group_following.append((following_score, user_id))
         
-        #if user_id not in user_group_map:
-            #user_group_map[user_id] = ([follower_score], [following_score])
-        #else:
-            #user_group_map[user_id][0].append(follower_score)
-            #user_group_map[user_id][1].append(following_score)
+        if user_id not in user_group_map:
+            user_group_map[user_id] = ([follower_score], [following_score])
+        else:
+            user_group_map[user_id][0].append(follower_score)
+            user_group_map[user_id][1].append(following_score)
 
     #TwitterGroup.upsert_group(i, group_followers, group_following, group_adjacency[i])
-#print "Done creating groups."
+print "Done creating groups."
     
 i = 0    
 for user_id, following_followedby_tup in user_group_map.items():
